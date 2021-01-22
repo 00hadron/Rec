c0a6efef8bb98ef80b505cb3d15a8f472eb0e383
Katya Popova <hadron@yandex.ru>
2021-01-22 03:10:00 +0300

    Add logo splash screen without extra Activity

--- app/src/main/java/ru/hadron/rec/ui/MainActivity.kt
+++ app/src/main/java/ru/hadron/rec/ui/MainActivity.kt
@@ -1,11 +1,13 @@
-package ru.hadron.rec
+package ru.hadron.rec.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.hadron.rec.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_AppTheme)
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/ru/hadron/rec/ui/fragments/RecListFragment.kt
@@ -0,0 +1,7 @@
+package ru.hadron.rec.ui.fragments
+
+import androidx.fragment.app.Fragment
+import ru.hadron.rec.R
+
+class RecListFragment : Fragment(R.layout.fragment_rec_list){
+}
