8868e264c634aa761627a157f9bd3ceecac118a9
Katya Popova <hadron@yandex.ru>
2021-01-22 05:42:22 +0300

    Add Bottom Navigation & FAB

--- app/src/main/java/ru/hadron/rec/ui/MainActivity.kt
+++ app/src/main/java/ru/hadron/rec/ui/MainActivity.kt
@@ -2,12 +2,22 @@ package ru.hadron.rec.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.hadron.rec.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_AppTheme)
+
         setContentView(R.layout.activity_main)
+        this.resolveUiProblems()
+
+    }
+
+    private fun resolveUiProblems() {
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(1).isEnabled = false
     }
 }
